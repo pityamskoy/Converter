e95c5f4b54b1bca44b40344047d4405671778cc9
ASKekishev <andreign@bk.ru>
2026-03-14 00:19:05 +0300

    feat(conversion): add untested version of .json -> .csv conversion

---
+++ src/main/java/team/anonyms/converter/controllers/ConversionController.java
@@ -0,0 +1,68 @@
+package team.anonyms.converter.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+import team.anonyms.converter.errors.UnsupportedExtensionException;
+import team.anonyms.converter.services.ConversionService;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/conversion")
+public final class ConversionController {
+    private final static Logger log = LoggerFactory.getLogger(ConversionController.class);
+
+    @Autowired
+    private ConversionService conversionService;
+
+    @PostMapping(value = "/json_csv", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<StreamingResponseBody> convertJsonFileToCsv(@RequestPart MultipartFile file) {
+        String fileName = file.getName();
+        log.info("Called convertJsonFileToCsv; filename={}", fileName);
+
+        if (!fileName.endsWith(".json")) {
+            log.error("File doesn't have '.json' extension");
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (file.isEmpty()) {
+            log.error("File is empty");
+            return ResponseEntity.badRequest().build();
+        }
+
+        try {
+            Path csvPath = conversionService.convertJsonFileToCsv(file);
+
+            StreamingResponseBody stream = outputStream -> {
+                try (InputStream in = Files.newInputStream(csvPath)) {
+                    in.transferTo(outputStream);
+                } finally {
+                    log.debug("Deleting temp file={}", csvPath);
+                    Files.deleteIfExists(csvPath);
+                }
+            };
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentDisposition(ContentDisposition.attachment().
+                    filename(csvPath.getFileName().toString()).build());
+            headers.setContentType(MediaType.parseMediaType("text/csv"));
+            headers.setContentLength(Files.size(csvPath));
+
+            return new ResponseEntity<>(stream, headers, HttpStatus.OK);
+        } catch (UnsupportedExtensionException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        } catch (IOException e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
+}
---
+++ src/main/java/team/anonyms/converter/errors/UnsupportedExtensionException.java
@@ -0,0 +1,19 @@
+package team.anonyms.converter.errors;
+
+public class UnsupportedExtensionException extends RuntimeException {
+    public UnsupportedExtensionException() {
+        super();
+    }
+
+    public UnsupportedExtensionException(String message) {
+        super(message);
+    }
+
+    public UnsupportedExtensionException(Throwable cause) {
+        super(cause);
+    }
+
+    public UnsupportedExtensionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
