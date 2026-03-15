b01212930de995ccf958c7cb1a571d36f4b023d4
ASKekishev <andreign@bk.ru>
2026-03-15 23:04:24 +0300

    feat(documentation): add LastSupportedProjectVersion annotation for documentation

---
+++ src/main/java/team/anonyms/converter/annotations/LastSupportedProjectVersion.java
@@ -0,0 +1,30 @@
+package team.anonyms.converter.annotations;
+
+import team.anonyms.converter.enums.ProjectVersion;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * <p>
+ *     {@code LastSupportedProjectVersion} is used to document what the last project version a target supports.
+ * </p>
+ * <p>
+ *     It is supposed to be used in seldom cases when a target totally relies on a project environment. <br>
+ *     For example, it is reasonable to point last supported project version when meaningfulness
+ *     of usage a target depends on configuration of the project.
+ * </p>
+ * <p>
+ *     Overall, it is highly recommended to use this annotation only in necessary cases, because
+ *     all targets annotated with this annotation should have new values after they are updated.
+ * </p>
+ */
+@Target(value = {TYPE, ANNOTATION_TYPE, METHOD, CONSTRUCTOR})
+@Retention(RUNTIME)
+public @interface LastSupportedProjectVersion {
+    ProjectVersion value();
+}
