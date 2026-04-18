bc1e1bdc87af2d1972f9b8fd94a93c71c3710de0
ASKekishev <andreign@bk.ru>
2026-04-18 17:36:51 +0300

    fix(errors): change access modifiers in ErrorResponse

--- src/main/java/team/anonyms/converter/dto/controller/responses/errors/ErrorResponse.java
+++ src/main/java/team/anonyms/converter/dto/controller/responses/errors/ErrorResponse.java
@@ -6,9 +6,9 @@ import java.time.Instant;
 
 @Data
 public abstract class ErrorResponse {
-    public int statusCode;
-    public String message;
-    public Instant time;
+    private int statusCode;
+    private String message;
+    private Instant time;
 
     protected ErrorResponse(int statusCode, String message) {
         this.statusCode = statusCode;
